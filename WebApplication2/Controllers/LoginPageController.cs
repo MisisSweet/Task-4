35cb52496b562325b01dca0a0e6a901ab4a4364b
MisisSweet <48993011+MisisSweet@users.noreply.github.com>
2021-02-11 12:32:43 +0300

    Добавьте файлы проекта.

---
+++ WebApplication2/Controllers/LoginPageController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Controllers
+{
+    public class LoginPageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult onTapLogin(string login)
+        {
+            return Redirect("~/RegisterPage");
+        }
+        public IActionResult onTapRegistration(string reg)
+        {
+            return Redirect("~/RegisterPage");
+        }
+    }
+}
